37bca9717429a0a34d284f81ee856feae40e0f59
SasukeXDev <srk100m@gmail.com>
2025-11-15 23:38:38 +0530

    Update render_template.py
    rfd

--- bot/server/render_template.py
+++ bot/server/render_template.py
@@ -27,61 +27,116 @@ hide_channel = """
                     </style>"""
 
 
-async def render_page(id, secure_hash, is_admin=False, html='', playlist='', database='', route='', redirect_url='', msg='', chat_id=''):
-    theme = await db.get_variable('theme')
-    if theme is None or theme == '':
+async def render_page(
+    id,
+    secure_hash,
+    is_admin=False,
+    html="",
+    playlist="",
+    database="",
+    route="",
+    redirect_url="",
+    msg="",
+    chat_id="",
+):
+    theme = await db.get_variable("theme")
+    if theme is None or theme == "":
         theme = Telegram.THEME
-    tpath = ospath.join('bot', 'server', 'template')
-    if route == 'login':
-        async with aiopen(ospath.join(tpath, 'login.html'), 'r') as f:
-            html = (await f.read()).replace("<!-- Error -->", msg or '').replace("<!-- Theme -->", theme.lower()).replace("<!-- RedirectURL -->", redirect_url)
-    elif route == 'home':
-        async with aiopen(ospath.join(tpath, 'home.html'), 'r') as f:
-            html = (await f.read()).replace("<!-- Print -->", html).replace("<!-- Theme -->", theme.lower()).replace("<!-- Playlist -->", playlist)
+    tpath = ospath.join("bot", "server", "template")
+    if route == "login":
+        async with aiopen(ospath.join(tpath, "login.html"), "r") as f:
+            html = (
+                (await f.read())
+                .replace("<!-- Error -->", msg or "")
+                .replace("<!-- Theme -->", theme.lower())
+                .replace("<!-- RedirectURL -->", redirect_url)
+            )
+    elif route == "home":
+        async with aiopen(ospath.join(tpath, "home.html"), "r") as f:
+            html = (
+                (await f.read())
+                .replace("<!-- Print -->", html)
+                .replace("<!-- Theme -->", theme.lower())
+                .replace("<!-- Playlist -->", playlist)
+            )
             if not is_admin:
                 html += admin_block
                 if Telegram.HIDE_CHANNEL:
                     html += hide_channel
-    elif route == 'playlist':
-        async with aiopen(ospath.join(tpath, 'playlist.html'), 'r') as f:
-            html = (await f.read()).replace("<!-- Theme -->", theme.lower()).replace("<!-- Playlist -->", playlist).replace("<!-- Database -->", database).replace("<!-- Title -->", msg).replace("<!-- Parent_id -->", id)
+    elif route == "playlist":
+        async with aiopen(ospath.join(tpath, "playlist.html"), "r") as f:
+            html = (
+                (await f.read())
+                .replace("<!-- Theme -->", theme.lower())
+                .replace("<!-- Playlist -->", playlist)
+                .replace("<!-- Database -->", database)
+                .replace("<!-- Title -->", msg)
+                .replace("<!-- Parent_id -->", id)
+            )
             if not is_admin:
                 html += admin_block
 
-  elif route == 'list':
-    async with aiopen(ospath.join(tpath, 'list.html'), 'r') as f:
-        html = (await f.read()).replace("<!-- Print -->", html).replace("<!-- Theme -->", theme.lower()).replace("<!-- Database -->", database) 
-                               
+    elif route == "list":
+        async with aiopen(ospath.join(tpath, "list.html"), "r") as f:
+            html = (
+                (await f.read())
+                .replace("<!-- Print -->", html)
+                .replace("<!-- Theme -->", theme.lower())
+                .replace("<!-- Title -->", msg)
+                .replace("<!-- Database -->", database)
+            )
+
     if not is_admin:
         html += admin_block
         if Telegram.HIDE_CHANNEL:
             html += hide_channel
-          
-    elif route == 'index':
-        async with aiopen(ospath.join(tpath, 'index.html'), 'r') as f:
-            html = (await f.read()).replace("<!-- Print -->", html)
-                                   .replace("<!-- Theme -->", theme.lower())
-                                   .replace("<!-- Title -->", msg)
-                                   .replace("<!-- Chat_id -->", chat_id)
+
+    elif route == "index":
+        async with aiopen(ospath.join(tpath, "index.html"), "r") as f:
+            html = (
+                (await f.read())
+                .replace("<!-- Print -->", html)
+                .replace("<!-- Theme -->", theme.lower())
+                .replace("<!-- Title -->", msg)
+                .replace("<!-- Chat_id -->", chat_id)
+            )
             if not is_admin:
                 html += admin_block
     else:
-        file_data = await get_file_ids(StreamBot, chat_id=int(chat_id), message_id=int(id))
+        file_data = await get_file_ids(
+            StreamBot, chat_id=int(chat_id), message_id=int(id)
+        )
         if file_data.unique_id[:6] != secure_hash:
-            LOGGER.info('Link hash: %s - %s', secure_hash,
-                        file_data.unique_id[:6])
-            LOGGER.info('Invalid hash for message with - ID %s', id)
+            LOGGER.info(
+                "Link hash: %s - %s", secure_hash, file_data.unique_id[:6]
+            )
+            LOGGER.info("Invalid hash for message with - ID %s", id)
             raise InvalidHash
-        filename, tag, size = file_data.file_name, file_data.mime_type.split(
-            '/')[0].strip(), get_readable_file_size(file_data.file_size)
+        filename, tag, size = (
+            file_data.file_name,
+            file_data.mime_type.split("/")[0].strip(),
+            get_readable_file_size(file_data.file_size),
+        )
         if filename is None:
             filename = "Proper Filename is Missing"
-        filename = re.sub(r'[,|_\',]', ' ', filename)
-        if tag == 'video':
-            async with aiopen(ospath.join(tpath, 'video.html')) as r:
+        filename = re.sub(r"[,|_\',]", " ", filename)
+        if tag == "video":
+            async with aiopen(ospath.join(tpath, "video.html")) as r:
                 poster = f"/api/thumb/{chat_id}?id={id}"
-                html = (await r.read()).replace('<!-- Filename -->', filename).replace("<!-- Theme -->", theme.lower()).replace('<!-- Poster -->', poster).replace('<!-- Size -->', size).replace('<!-- Username -->', StreamBot.me.username)
+                html = (
+                    (await r.read())
+                    .replace("<!-- Filename -->", filename)
+                    .replace("<!-- Theme -->", theme.lower())
+                    .replace("<!-- Poster -->", poster)
+                    .replace("<!-- Size -->", size)
+                    .replace("<!-- Username -->", StreamBot.me.username)
+                )
         else:
-            async with aiopen(ospath.join(tpath, 'dl.html')) as r:
-                html = (await r.read()).replace('<!-- Filename -->', filename).replace("<!-- Theme -->", theme.lower()).replace('<!-- Size -->', size)
+            async with aiopen(ospath.join(tpath, "dl.html")) as r:
+                html = (
+                    (await r.read())
+                    .replace("<!-- Filename -->", filename)
+                    .replace("<!-- Theme -->", theme.lower())
+                    .replace("<!-- Size -->", size)
+                )
     return html
